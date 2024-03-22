8a74f67da8ccc944eed0a336798c1d1ecb99434a
maria jose <mariajdab@gmail.com>
2024-03-21 23:39:02 -0400

    rename directories

--- internal/customers/repository.go
+++
@@ -1,4 +0,0 @@
-package customers
-
-type Repository interface {
-}
--- internal/customers/usecase.go
+++
@@ -1,7 +0,0 @@
-package customers
-
-import "github.com/mariajdab/pay-gateway/internal/entity"
-
-type UseCase interface {
-	CreatePaymentRequest(txn entity.Transaction)
-}
