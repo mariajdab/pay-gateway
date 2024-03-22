5234d5d34686a0fa00b24f07a79476189d018468
maria jose <mariajdab@gmail.com>
2024-03-22 18:33:24 -0400

    update repository processor

--- internal/payProcessor/repository.go
+++ internal/payProcessor/repository.go
@@ -3,9 +3,10 @@ package payProcessor
 import "github.com/mariajdab/pay-gateway/internal/entity"
 
 type Repository interface {
-	AddPaymentCardDetail(card entity.Card, cardTokenUUID, cardBankUUID string) error
-	AddPaymentTxnHistory(txn entity.Transaction) error
+	AddPaymentCardDetail(card entity.Card, cardTk, cardBankUUID string) error
+	PaymentCardInfoExists(cardTk string) (bool, error)
+	AddPaymentTxnHistory(txn entity.Transaction, cardToken string) error
 	GetPaymentTxnStatus(txnUUID string) (string, error)
 	UpdatePaymentTxnStatus(txnUUID, status string) error
-	GetPaymentCardBankUUID(cardUUIDtk string) (id string, err error)
+	GetPaymentCardBankUUID(cardTk string) (id string, err error)
 }
--- internal/payProcessor/repository/postgres/processor.go
+++ internal/payProcessor/repository/postgres/processor.go
@@ -17,15 +17,44 @@ func NewPaymentProcessor(conn *pgxpool.Pool) payProcessor.Repository {
 	return &paymentProcessorRepo{conn: conn}
 }
 
-func (p *paymentProcessorRepo) AddPaymentTxnHistory(txn entity.Transaction) error {
+// AddPaymentCardDetail store only safe information about the payment card. TODO : we can encrypt card expiration date
+func (p *paymentProcessorRepo) AddPaymentCardDetail(card entity.Card, cardToken, cardBankUUID string) error {
 	ctx := context.Background()
 
 	_, err := p.conn.Exec(ctx, `
-		INSERT INTO payment_processor_hist (txn_uuid, amount, currency, country, card_token, created_at, purc_merchant_code, status_txn)
+		INSERT INTO card_processor (card_token, exp_date, card_bank_uuid, card_address)
+		VALUES ($1, $2, $3, $4)
+			 `, cardToken, card.ExpDate, cardBankUUID, card.Address)
+
+	return err
+}
+
+func (p *paymentProcessorRepo) PaymentCardInfoExists(cardTk string) (bool, error) {
+	var exists bool
+
+	ctx := context.Background()
+
+	if err := p.conn.QueryRow(ctx, `
+    SELECT EXISTS(SELECT 1 FROM card_processor WHERE card_token = $1)
+  `, cardTk).Scan(&exists); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return exists, nil
+}
+
+func (p *paymentProcessorRepo) AddPaymentTxnHistory(txn entity.Transaction, cardTk string) error {
+	ctx := context.Background()
+
+	_, err := p.conn.Exec(ctx, `
+		INSERT INTO payment_processor_hist (txn_uuid, amount, currency, country, card_token, created_at, merchant_code, status_txn)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 			 `,
-		txn.TxnUUID, txn.BillingAmount, txn.Currency,
-		txn.Country, txn.CardUUIDtk, txn.CratedAt,
+		txn.TxnUUID, txn.PaymentReq.BillingAmount, txn.PaymentReq.Currency,
+		txn.PaymentReq.Country, cardTk, txn.PaymentReq.CratedAt,
 		txn.MerchantCode, txn.Status)
 
 	return err
@@ -74,15 +103,4 @@ func (p *paymentProcessorRepo) GetPaymentCardBankUUID(cardUUIDtk string) (id str
 	}
 
 	return cardUUIDbank, nil
-}
-
-func (p *paymentProcessorRepo) AddPaymentCardDetail(card entity.Card, cardTokenUUID, cardBankUUID string) error {
-	ctx := context.Background()
-
-	_, err := p.conn.Exec(ctx, `
-		INSERT INTO card_processor (card_uuid_token, last4_digits, exp_date, card_bank_uuid, card_address)
-		VALUES ($1, $2, $3, $4)
-			 `, cardTokenUUID, card.Last4Digits, card.ExpDate, cardBankUUID, card.Address)
-
-	return err
 }
