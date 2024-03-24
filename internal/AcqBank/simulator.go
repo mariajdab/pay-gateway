db3389496fcd8ade8b64fbf33a1ed2b7d9bf780d
maria jose <mariajdab@gmail.com>
2024-03-24 16:30:11 -0400

    simulator: improvements and adding tests

--- internal/AcqBank/simulator.go
+++ internal/AcqBank/simulator.go
@@ -1,34 +1,33 @@
 package AcqBank
 
 import (
-	"math/rand"
-	"net/http"
-	"strconv"
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/mariajdab/pay-gateway/internal/entity"
+	"net/http"
+	"strconv"
 )
 
 const (
-	// txn statuses
-	allowed  = "txn-allowed"
-	declined = "txn-declined"
-	pending  = "txn-pending"
+	approved = 2020
+	declined = 9999
+	pending  = 8585
 )
 
-var txnStatusMap = map[int]string{
-	0: allowed,
-	1: pending,
-	2: declined,
+type CheckTxResp struct {
+	Status uint16 `json:"status_txn"`
+}
+
+type CardValidResp struct {
+	Valid    bool   `json:"valid"`
+	CardUUID string `json:"card_uuid"`
 }
 
 type Handler struct{}
 
 func RegisterHTTPEndpoints(g *gin.Engine, handler *Handler) {
 
-	payment := g.Group("/bank-sim")
+	payment := g.Group("/bank-sim.com")
 	{
 		payment.GET("/cards/:card_uuid", handler.checkTxn)
 		payment.GET("/cards/", handler.validateCard)
@@ -43,14 +42,11 @@ func (h *Handler) checkTxn(ctx *gin.Context) {
 		return
 	}
 
-	rand.Seed(time.Now().UnixNano())
-	randomTxStatus := rand.Intn(3)
+	status := getTxnStatus(cardUUID)
 
-	txnStatus := struct {
-		StatusTxn string `json:"status_txn"`
-	}{StatusTxn: txnStatusMap[randomTxStatus]}
+	statusTxn := CheckTxResp{status}
 
-	ctx.JSON(http.StatusOK, txnStatus)
+	ctx.JSON(http.StatusOK, statusTxn)
 
 }
 
@@ -68,18 +64,52 @@ func (h *Handler) validateCard(ctx *gin.Context) {
 	lastNumb, err := strconv.Atoi(lastNumberStr)
 	if err != nil {
 		ctx.AbortWithStatus(http.StatusInternalServerError)
+		return
 	}
 
+	cardResp := CardValidResp{}
 	// if last number >= 5, the card is a customer bank card valid, then
 	// response OK with the cardUUID for later communication between
 	// the payment gateway and the bank
 	if lastNumb >= 5 {
-		cardUUID := struct {
-			CardUUID string `json:"card_uuid"`
-		}{CardUUID: uuid.New().String()}
+		cardResp.CardUUID = uuid.New().String()
+		cardResp.Valid = true
 
-		ctx.JSON(http.StatusOK, cardUUID)
+		ctx.JSON(http.StatusOK, cardResp)
 	} else {
-		ctx.JSON(http.StatusUnauthorized, nil)
+		cardResp.Valid = false
+		ctx.JSON(http.StatusOK, cardResp)
 	}
+}
+
+func getTxnStatus(cardUUID string) uint16 {
+	lastCharCard := string(cardUUID[len(cardUUID)-1])
+
+	if isLastCharacterInt(lastCharCard) {
+		if isOddNumber(lastCharCard) {
+			return pending
+		} else {
+			return declined
+		}
+	}
+	// if the last character in the uuid is a letter, then approve the txn
+	return approved
+}
+
+func isLastCharacterInt(lastChar string) bool {
+	// if there is an error it should be character and not a number
+	if _, err := strconv.Atoi(lastChar); err == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isOddNumber(lastChar string) bool {
+	// should not be an error
+	v, _ := strconv.Atoi(lastChar)
+	if v%2 > 0 {
+		return true
+	}
+	return false
 }
--- internal/AcqBank/simulator_test.go
+++ internal/AcqBank/simulator_test.go
@@ -1,23 +1,121 @@
 package AcqBank
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/mariajdab/pay-gateway/internal/entity"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-func TestCheckTxn(t *testing.T) {
+func TestCheckTxnApproved(t *testing.T) {
 	r := gin.Default()
 
 	RegisterHTTPEndpoints(r, &Handler{})
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/bank-sim.com/cards/245-3646-7477", nil)
+	// the cardUUID final char is a letter, so the txn is approved
+	req, _ := http.NewRequest("GET", "/bank-sim.com/cards/3e2a16c7-002b-4e1a-9f12-75d4bfd9832c", nil)
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	fmt.Println(w.Body.String())
+
+	statusTx := &CheckTxResp{}
+
+	err := json.Unmarshal(w.Body.Bytes(), statusTx)
+	assert.Equal(t, err, nil)
+
+	// 2020 status approved
+	assert.Equal(t, statusTx.Status, uint16(2020))
+}
+
+func TestCheckTxnDeclined(t *testing.T) {
+	r := gin.Default()
+
+	RegisterHTTPEndpoints(r, &Handler{})
+
+	w := httptest.NewRecorder()
+	// the cardUUID final char is an int and is even, so the txn is declined
+	req, _ := http.NewRequest("GET", "/bank-sim.com/cards/3e2a16c7-002b-4e1a-9f12-75d4bfd98322", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	statusTx := &CheckTxResp{}
+
+	err := json.Unmarshal(w.Body.Bytes(), statusTx)
+	assert.Equal(t, err, nil)
+
+	// 9999 status declined
+	assert.Equal(t, statusTx.Status, uint16(9999))
+}
+
+func TestValidateCardOK(t *testing.T) {
+	r := gin.Default()
+
+	RegisterHTTPEndpoints(r, &Handler{})
+
+	cardBody := &entity.Card{
+		// the final number is >= 5, so the card should be valid
+		Number:  "28942958",
+		CVV:     123,
+		ExpDate: time.Now().Add(time.Hour * 24 * 10),
+	}
+
+	body, err := json.Marshal(cardBody)
+	assert.NoError(t, err)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/bank-sim.com/cards/", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	cardResp := &CardValidResp{}
+
+	err = json.Unmarshal(w.Body.Bytes(), cardResp)
+	assert.NoError(t, err)
+
+	// if the card is valid the response should have the cardUUID of len 36
+	assert.Equal(t, len(cardResp.CardUUID), 36)
+
+	// valid field should be true
+	assert.Equal(t, cardResp.Valid, true)
+}
+
+func TestValidateCardWrong(t *testing.T) {
+	r := gin.Default()
+
+	RegisterHTTPEndpoints(r, &Handler{})
+
+	cardBody := &entity.Card{
+		// the final number is >= 5, so the card should be valid
+		Number:  "28942954",
+		CVV:     123,
+		ExpDate: time.Now().Add(time.Hour * 24 * 10),
+	}
+
+	body, err := json.Marshal(cardBody)
+	assert.NoError(t, err)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/bank-sim.com/cards/", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	cardResp := &CardValidResp{}
+
+	err = json.Unmarshal(w.Body.Bytes(), cardResp)
+	assert.NoError(t, err)
+
+	// if the card is not valid the response should not have the cardUUID
+	assert.Equal(t, len(cardResp.CardUUID), 0)
+
+	// valid field should be false
+	assert.Equal(t, cardResp.Valid, false)
 }
