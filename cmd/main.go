691932d992be9cc701f55304e186ef5d86237c5e
maria jose <mariajdab@gmail.com>
2024-03-26 09:47:45 -0400

    update main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-const port = 9000
+import (
+	"github.com/mariajdab/pay-gateway/internal/server"
+	"log"
+)
+
+const port = "8080"
 
 func main() {
+	app := server.NewApp()
 
+	if err := app.Run(port); err != nil {
+		log.Fatal("Failed to listen and serve: ", err)
+	}
 }
